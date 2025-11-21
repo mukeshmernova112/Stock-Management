6397701079155e3793dc0b92a2dacd4fa4d0770d
Mukeshkanna M <mukeshmernova@gmail.com>
2025-11-21 12:47:47 +0530

    render update

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:5000/api",
+  deploymentBaseURL: "https://stock-management-1-v9hz.onrender.com/api",
 });
 
 // Attach token automatically
